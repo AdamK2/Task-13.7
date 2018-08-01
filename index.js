62ee6b5b3bd0d1273b27139e159ed22b35c06cdc
Adam Kulpa <ad200@gazeta.pl>
2018-08-01 17:20:21 +0100

    Change js

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-// Import modułu "OSinfo":
-var OSinfo = require('./modules/OSInfo');
+var fs = require('fs');
+var colors = require('colors');
 
-
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-			case '/getOSinfo':
-				// Użycie zaimportowanych danych z modułu "OSinfo".
-				OSinfo.print();				
-				break;					
-			case '/getOStime':
-				// Użycie zaimportowanych danych z modułu "OStime".
-				OSinfo.getOStime2();				
-				break;				
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
-    }
+fs.readdir('./modules', 'utf-8', function(err, data) {
+    console.log('Pliki przed zapisem!'.red);
+    console.log(data);
+    fs.writeFile('./tekst.txt', data, function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.red);          
+    });
 });
-
-
 
 
